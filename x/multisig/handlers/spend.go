ad552eb9b30a84d3b7cb2a53f706dc9cdf623676
Ashwin Phatak <ashwinpphatak@gmail.com>
2018-12-17 18:15:37 +0530

    Spend msg handler.

--- x/multisig/handlers/spend.go
+++ x/multisig/handlers/spend.go
@@ -11,5 +11,30 @@ import (
 
 // Handle MsgSpendMultiSig.
 func handleMsgSpendMultiSig(ctx sdk.Context, keeper Keeper, msg msgs.MsgSpendMultiSig) sdk.Result {
+	if !keeper.HasContract(ctx, msg.ID) {
+		return sdk.ErrInternal("Contract not found.").Result()
+	}
+
+	obj := keeper.GetContract(ctx, msg.ID)
+	if !obj.AliceAddress.Equals(msg.AliceAddress) || !obj.BobAddress.Equals(msg.BobAddress) {
+		return sdk.ErrInternal("Address not authorized to spend.").Result()
+	}
+
+	if obj.State != StateLocked {
+		return sdk.ErrInternal("Contract counterparty hasn't joined, can't spend.").Result()
+	}
+
+	if obj.Balance.IsLT(msg.Amount) {
+		return sdk.ErrInsufficientCoins("Not enough coins.").Result()
+	}
+
+	_, _, err := keeper.coinKeeper.AddCoins(ctx, msg.ToAddress, sdk.Coins{msg.Amount})
+	if err != nil {
+		return sdk.ErrInternal("Error transferring coins.").Result()
+	}
+
+	obj.Balance = obj.Balance.Minus(msg.Amount)
+	keeper.UpsertContract(ctx, obj)
+
 	return sdk.Result{}
 }
