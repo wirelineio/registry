8513f552cd21225a5efc380b08546e92bedea606
Ashwin Phatak <ashwinpphatak@gmail.com>
2019-03-14 16:57:14 +0530

    Tx broadcast to commit GQL API.

--- x/registry/gql/resolver.go
+++ x/registry/gql/resolver.go
@@ -4,20 +4,33 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 
 	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	abci "github.com/tendermint/tendermint/abci/types"
+	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
+	"github.com/tendermint/tendermint/rpc/core"
+	ctypes "github.com/tendermint/tendermint/rpc/core/types"
 	"github.com/wirelineio/wirechain/x/registry"
 )
 
 // Resolver is the GQL query resolver.
 type Resolver struct {
 	baseApp       *bam.BaseApp
+	codec         *codec.Codec
 	keeper        registry.Keeper
 	accountKeeper auth.AccountKeeper
 }
+
+// Mutation is the entry point to tx execution.
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+}
+
+type mutationResolver struct{ *Resolver }
 
 // Query is the entry point to query execution.
 func (r *Resolver) Query() QueryResolver {
@@ -25,6 +38,23 @@ func (r *Resolver) Query() QueryResolver {
 }
 
 type queryResolver struct{ *Resolver }
+
+func (r *mutationResolver) BroadcastTxCommit(ctx context.Context, tx string) (*string, error) {
+
+	stdTx, err := decodeStdTx(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := broadcastTx(r, stdTx)
+	if err != nil {
+		return nil, err
+	}
+
+	txHash := res.Hash.String()
+
+	return &txHash, nil
+}
 
 func (r *queryResolver) GetAccounts(ctx context.Context, addresses []string) ([]*Account, error) {
 	accounts := make([]*Account, len(addresses))
@@ -63,7 +93,7 @@ func (r *queryResolver) GetAccount(ctx context.Context, address string) (*Accoun
 	}
 
 	account := r.accountKeeper.GetAccount(sdkContext, addr)
-	if account == nil {
+	if account == nil || account.GetPubKey() == nil {
 		return nil, nil
 	}
 
@@ -170,4 +200,119 @@ func mapToJSONStr(attrs map[string]interface{}) (*string, error) {
 	attrsJSONStr := string(attrsJSON)
 
 	return &attrsJSONStr, nil
+}
+
+func decodeStdTx(tx string) (*auth.StdTx, error) {
+	bytes, err := base64.StdEncoding.DecodeString(tx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Note: json.Unmarshal doesn't known which Msg struct to use, so we do it "manually".
+	// See https://stackoverflow.com/questions/11066946/partly-json-unmarshal-into-a-map-in-go
+	var objmap map[string]*json.RawMessage
+	err = json.Unmarshal(bytes, &objmap)
+	if err != nil {
+		return nil, err
+	}
+
+	var msg []registry.MsgSetResource
+	err = json.Unmarshal(*objmap["msg"], &msg)
+	if err != nil {
+		return nil, err
+	}
+
+	var fee auth.StdFee
+	err = json.Unmarshal(*objmap["fee"], &fee)
+	if err != nil {
+		return nil, err
+	}
+
+	var sigs []*json.RawMessage
+	err = json.Unmarshal(*objmap["signatures"], &sigs)
+	if err != nil {
+		return nil, err
+	}
+
+	var sig map[string]*json.RawMessage
+	err = json.Unmarshal(*sigs[0], &sig)
+	if err != nil {
+		return nil, err
+	}
+
+	var pubKeyStr string
+	err = json.Unmarshal(*sig["pub_key"], &pubKeyStr)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKeyBytes, err := base64.StdEncoding.DecodeString(pubKeyStr)
+	if err != nil {
+		return nil, err
+	}
+
+	pubKey, err := cryptoAmino.PubKeyFromBytes(pubKeyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	var signature []byte
+	err = json.Unmarshal(*sig["signature"], &signature)
+	if err != nil {
+		return nil, err
+	}
+
+	var accountNum uint64
+	err = json.Unmarshal(*sig["account_number"], &accountNum)
+	if err != nil {
+		return nil, err
+	}
+
+	var sequenceNum uint64
+	err = json.Unmarshal(*sig["sequence"], &sequenceNum)
+	if err != nil {
+		return nil, err
+	}
+
+	var memo string
+	err = json.Unmarshal(*objmap["memo"], &memo)
+	if err != nil {
+		return nil, err
+	}
+
+	stdTx := auth.StdTx{
+		Msgs: []sdk.Msg{msg[0]},
+		Fee:  fee,
+		Signatures: []auth.StdSignature{auth.StdSignature{
+			PubKey:        pubKey,
+			Signature:     signature,
+			AccountNumber: accountNum,
+			Sequence:      sequenceNum,
+		}},
+		Memo: memo,
+	}
+
+	return &stdTx, nil
+}
+
+func broadcastTx(r *mutationResolver, stdTx *auth.StdTx) (*ctypes.ResultBroadcastTxCommit, error) {
+	txBytes, err := r.Resolver.codec.MarshalBinaryLengthPrefixed(stdTx)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := core.BroadcastTxCommit(txBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.CheckTx.IsErr() {
+		return nil, errors.New(res.CheckTx.String())
+	}
+
+	if res.DeliverTx.IsErr() {
+		return nil, errors.New(res.DeliverTx.String())
+	}
+
+	return res, nil
 }
